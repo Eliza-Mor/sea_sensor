32d2a52698ea3de9037895219b6ace5b661c7613
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-04-10 08:17:17 -0500

    Ya obtiene los datos la API, pero no me permite mapearlos

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,42 @@
-// app.js
 const express = require('express');
 const mongoose = require('mongoose');
-const sensorRoutes = require('./routes/sensorRoutes');
-require('dotenv').config();
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conexión exitosa a la base de datos MongoDB'))
-    .catch(err => console.error('Error de conexión a la base de datos MongoDB:', err.message));
+// Conexión a la base de datos MongoDB Atlas
+const mongoUri = 'mongodb+srv://admin:2822004@ioty.ikxu0cy.mongodb.net/iot';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-app.use(express.json());
-app.use(sensorRoutes);
+// Verificar la conexión a MongoDB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
+db.once('open', () => {
+    console.log('Conexión establecida correctamente con MongoDB Atlas');
+});
 
+// Definir un esquema y un modelo para la colección 'sensores'
+const sensorSchema = new mongoose.Schema({
+    pH: Number,
+    temperature: Number
+}, { collection: 'sensores' }); // Especificar el nombre de la colección aquí
+
+const Sensor = mongoose.model('Sensor', sensorSchema);
+
+// Ruta para obtener datos de sensores
+app.get('/sensores', async (req, res) => {
+    try {
+        const sensores = await Sensor.find({}, 'pH temperature');
+        res.json(sensores);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener los datos de sensores.' });
+    }
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000; // Utilizar el puerto definido por el entorno o 3000 por defecto
 app.listen(PORT, () => {
-    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+    console.log(`Servidor de API RESTful en ejecución en http://localhost:${PORT}`);
 });
--- seaSensor/App.js
+++ seaSensor/App.js
@@ -1,96 +1,127 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text,TouchableOpacity, View, ImageBackground, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, TouchableOpacity, View, ImageBackground, Dimensions } from 'react-native';
 import tw from 'twrnc';
 import { LinearGradient } from 'expo-linear-gradient';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
 
 export default function App() {
   const windowHeight = Dimensions.get('window').height;
 
+  const [pH, setPH] = useState('');
+  const [temperature, setTemperature] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/sensores');
+        const data = response.data;
+        // Actualiza los estados locales con los datos recibidos
+        if (data.length > 0) {
+          const { pH, temperature } = data[0];
+          setPH(pH.toString());
+          setTemperature(temperature.toString());
+        } else {
+          console.warn('La respuesta no contiene datos válidos.');
+        }
+      } catch (error) {
+        console.error('Error fetching sensor data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
 
   return (
     <View style={tw`flex-1`}>
-      
+
       <ImageBackground
         source={require('./assets/SEASENSOR.png')}
         style={[tw`flex-1`, tw`justify-center items-center`]}
-      > 
+      >
       </ImageBackground>
 
       <LinearGradient
-        colors={['transparent', '#0353a4']} 
+        colors={['transparent', '#0353a4']}
         style={tw`flex-1`}
       >
-         <View style={[tw`absolute`, { 
-        bottom: windowHeight / 2.4, 
-        left: 40,
-        right: 40,
-      }]}>
-        <LinearGradient
-          colors={['rgba(196, 244, 223, 90)', 'transparent']} 
-          style={tw`p-6 rounded-lg shadow-md`}
+
+        <View style={[tw`absolute`, {
+          bottom: windowHeight / 2.4,
+          left: 40,
+          right: 40,
+        }]}>
+          <LinearGradient
+            colors={['rgba(196, 244, 223, 90)', 'transparent']}
+            style={tw`p-6 rounded-lg shadow-md`}
+          >
+            <View style={[tw`h-28`, { width: '100%', flexDirection: 'column', justifyContent: 'space-between' }]}>
+              <Text style={tw`text-black text-3xl font-bold text-center`}>
+                PH: <Text style={tw`text-black text-3xl font-semibold text-center`}>{pH}</Text>
+              </Text>
+              <Text style={tw`text-black text-3xl font-bold text-center`}>
+                Temperatura: <Text style={tw`text-black text-3xl font-semibold text-center`}>{temperature}°C</Text>
+              </Text>
+
+            </View>
+          </LinearGradient>
+        </View>
+
+
+        <TouchableOpacity
+          onPressIn={() => {
+            console.log("Inicio de la lectura de datos");
+          }}
+          onPressOut={() => {
+            console.log("Fin de la lectura de datos");
+          }}
+          style={[tw`absolute`, { bottom: windowHeight / 4, left: 150, right: 165 }]}
         >
-          <View style={[tw`h-28`, { width: '100%', flexDirection: 'column', justifyContent: 'space-between' }]}>
-            <Text style={tw`text-black text-3xl font-bold text-center`}>PH:<Text style={tw`text-black text-3xl font-semibold text-center`}>{"  "}20</Text></Text>
-            <Text style={tw`text-black text-3xl font-bold text-center`}>Temperatura:<Text style={tw`text-black text-3xl font-semibold text-center`}>{"  "}20 °C</Text></Text>
+          <View
+            style={[tw`bg-white p-4 rounded-lg shadow-md h-15 flex items-center justify-center`, { width: '150%', backgroundColor: '#90E2F5' }]}
+          >
+            <FontAwesomeIcon icon={faArrowUp} size={40} color="#DBA41A" />
           </View>
-        </LinearGradient>
-      </View>
+        </TouchableOpacity>
 
-      <TouchableOpacity 
-  onPressIn={() => {
-    console.log("Inicio de la lectura de datos");
-  }}
-  onPressOut={() => {
-    console.log("Fin de la lectura de datos");
-  }}
-  style={[tw`absolute`, { bottom: windowHeight / 4, left: 180, right: 180 }]}
->
-  <View 
-    style={[tw`bg-white p-4 rounded-lg shadow-md h-20 flex items-center justify-center`, { width: '150%', backgroundColor: '#90E2F5' }]}
-  >
-    <FontAwesomeIcon icon={faArrowUp} size={50} color="#DBA41A"/>
-  </View>
-</TouchableOpacity>
+        <TouchableOpacity
+          onPressIn={() => {
+            console.log("Inicio de la lectura de datos");
+          }}
+          onPressOut={() => {
+            console.log("Fin de la lectura de datos");
+          }}
+          style={[tw`absolute`, { bottom: windowHeight / 7, left: 145, right: 170 }]}
+        >
+          <View
+            style={[tw`bg-white p-4 rounded-lg shadow-md h-15 flex items-center justify-center`, { width: '155%', right: '155%', backgroundColor: '#90E2F5' }]}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} size={40} color="#DBA41A" />
+          </View>
+        </TouchableOpacity>
 
+        <TouchableOpacity
+          onPressIn={() => {
+            console.log("Inicio de la lectura de datos");
+          }}
+          onPressOut={() => {
+            console.log("Fin de la lectura de datos");
+          }}
+          style={[tw`absolute`, { bottom: windowHeight / 7, left: 150, right: 165 }]}
+        >
+          <View
+            style={[tw`bg-white p-4 rounded-lg shadow-md h-15 flex items-center justify-center`, { width: '150%', left: '155%', backgroundColor: '#90E2F5' }]}
+          >
+            <FontAwesomeIcon icon={faArrowRight} size={40} color="#DBA41A" />
+          </View>
+        </TouchableOpacity>
 
-<TouchableOpacity 
-  onPressIn={() => {
-    console.log("Inicio de la lectura de datos");
-  }}
-  onPressOut={() => {
-    console.log("Fin de la lectura de datos");
-  }}
-  style={[tw`absolute`, { bottom: windowHeight / 7, left: 180, right: 180 }]}
->
-  <View 
-    style={[tw`bg-white p-4 rounded-lg shadow-md h-20 flex items-center justify-center`, { width: '150%', right: '160%', backgroundColor: '#90E2F5' }]}
-  >
-    <FontAwesomeIcon icon={faArrowLeft} size={50} color="#DBA41A"/>
-  </View>
-</TouchableOpacity>
-
-
-<TouchableOpacity 
-  onPressIn={() => {
-    console.log("Inicio de la lectura de datos");
-  }}
-  onPressOut={() => {
-    console.log("Fin de la lectura de datos");
-  }}
-  style={[tw`absolute`, { bottom: windowHeight / 7, left: 180, right: 180 }]}
->
-  <View 
-    style={[tw`bg-white p-4 rounded-lg shadow-md h-20 flex items-center justify-center`, { width: '150%', left: '160%', backgroundColor: '#90E2F5' }]}
-  >
-    <FontAwesomeIcon icon={faArrowRight} size={50} color="#DBA41A"/>
-  </View>
-</TouchableOpacity>
       </LinearGradient>
-      
+
       <StatusBar style="light" />
     </View>
   );
